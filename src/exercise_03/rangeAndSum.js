11dad494422946489eaa8bef4f5111f85e6c79ec
emmeem <1124782639@qq.com>
2020-07-27 20:05:49 +0800

    junbin liao feat:exercise_03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,20 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (start === end) {
+    return [];
+  }
+  let ra = [];
+  if (start < end) {
+    for (let i = start; i < end; i += 1) {
+      ra.push(i);
+    }
+  } else {
+    for (let i = start; i > end; i -= 1) {
+      ra.push(i);
+    }
+  }
+  return ra;
 }
 
 export function sum(...numbers) {
@@ -17,6 +29,12 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (numbers === undefined) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+  }
+  return sum;
 }
