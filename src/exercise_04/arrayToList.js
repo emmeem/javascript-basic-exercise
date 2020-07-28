a89366210093cc413e0d44a4f9b2ee46b701b2d3
emmeem <1124782639@qq.com>
2020-07-28 13:06:56 +0800

    junbin liao feat:arrayToList

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,17 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  let head = { value: array[0], next: null };
+  let obj = head;
+  for (let i = 1; i < array.lenth; i += 1) {
+    obj.next = { value: array[i], next: null };
+    obj = obj.next;
+  }
+  return head;
 }
